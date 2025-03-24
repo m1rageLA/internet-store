ead1b2099dbb6fb87f2b1904aa6aaf36e13e9e39
Alex Wróbel <alexisooo@MacBook-Air-Alex.local>
2025-03-24 09:52:18 +0100

    add Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Counter from "./Counter.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-// js
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div>
@@ -29,8 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
