1ed5df8c1a5f8eeab36b49b4f33e79d89952689a
Yelyashevich_Yauheni <ajmeni56789@gmail.com>
2025-03-23 22:28:04 -0400

    Created Counter component with increment/decrement buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent from './components/MyComponent';
+import MyComponent from './components/MyComponent'
+import Counter from './components/Counter';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <MyComponent />
+      <Counter />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
