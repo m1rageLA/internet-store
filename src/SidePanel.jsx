9edd9c848e7d45659c666722edf570df53d9710f
Rafał <caluch22870@stud.akademiabialska.pl>
2025-03-31 10:38:46 +0200

    Dodano range slider, listę checkboxów

--- src/SidePanel.jsx
+++ src/SidePanel.jsx
@@ -1,11 +1,50 @@
-import React, {useState} from "react";
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import Container from '@mui/material/Container';
+import Slider from '@mui/material/Slider';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 function SidePanel() {
+    const [priceRange, setPriceRange] = useState([0, 100]);
+    const updateRange = (e, data) => {
+        setPriceRange(data);
+    };
+    const MAX = 100;
+    const MIN = 0;
+    const marks = [
+        {
+            value: MIN,
+            label: '',
+        },
+        {
+            value: MAX,
+            label: '',
+        },
+    ];
+    const categories = [
+        "Obuwie",
+        "Koszulki",
+        "Spodnie",
+    ];
+
     return (
-        <div>
-            <Button variant="contained">Hello world</Button>
-        </div>
+        <Container maxWidth="sm">
+            <span>Zakres cenowy</span>
+            <br /><br />
+            <Slider
+                marks={marks}
+                value={priceRange}
+                onChange={updateRange}
+                valueLabelDisplay="on"
+            />
+            <span>Kategorie</span>
+            <FormGroup>
+                {categories.map(category => (
+                    <FormControlLabel control={<Checkbox />} label={category} />
+                ))}
+            </FormGroup>
+        </Container>
     );
 }
 
