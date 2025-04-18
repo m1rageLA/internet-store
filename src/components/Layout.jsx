478d88c41403442c069d689760ef0affc26010ef
m1rage <miragela.code@gmail.com>
2025-04-18 18:38:39 +0200

    Remove unused Navbar import from Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header';
-import Navbar from './Navbar';
 import SidePanel from './SidePanel';
 import Footer from './Footer';
 
@@ -8,7 +7,6 @@ export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen font-sans w-full">
       <Header />
-      <Navbar />
       <div className="flex flex-1 w-full gap-6 px-4 py-6">
         <main className="flex-1">{children}</main>
         <SidePanel />
