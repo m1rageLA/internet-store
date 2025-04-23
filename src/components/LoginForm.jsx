9cee96c85ed66db5b8de06702cd561d08dba8efa
Hennnesssy <ruslanshatkun@gmail.com>
2025-04-23 16:13:09 +0200

    Add LoginForm component

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LoginForm() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
