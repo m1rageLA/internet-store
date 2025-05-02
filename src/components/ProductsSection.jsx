2b2379f3012edc0e136952ebec4b6c83018388e2
Rafał <caluch22870@stud.akademiabialska.pl>
2025-05-02 14:00:22 +0200

    Filtering products by category

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import { Grid, Typography, Container } from '@mui/material';
 import ProductCard from './ProductCard';
 import { products } from '../data/products';
+import { useCategory } from './CategoryContext';
 
 export default function ProductsSection() {
+
+  const { selected } = useCategory();
+
+  const filtered = selected.length === 0
+    ? products // Show all if none selected
+    : products.filter((p) => selected.includes(p.category));
+
   return (
     <Container maxWidth="lg" sx={{ mt: 4 }}>
       <Typography color='#2e2e2e' variant="h4" fontWeight="bold" gutterBottom>
         Produkty
       </Typography>
       <Grid container spacing={3}>
-        {products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} key={product.title}>
+        {filtered.map((product) => (
+          <Grid item xs={12} sm={6} md={4} key={product.id}>
             <ProductCard {...product} />
           </Grid>
         ))}
