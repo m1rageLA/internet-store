23ee29b32bfbdd1490bb33db5adb466f9601dbe7
m1rage <miragela.code@gmail.com>
2025-05-03 12:56:53 +0200

    Integrate Firebase Hosting and SDK
    - Updated package.json to include Firebase as a dependency.
    - Added Firebase configuration and initialization in src/firebase.js.
    - Created public/index.html for Firebase Hosting with necessary scripts.
    - Configured firebase.json for hosting settings and rewrites.
    - Added GitHub Actions workflows for deploying to Firebase on merge and pull request.
    - Created .firebaserc for project configuration.
    - Added hosting cache file for Firebase.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHrSa1uxOtgmmlLpQkgkpkTzMmfU3x6Bs",
+  authDomain: "internet-store-d7a86.firebaseapp.com",
+  projectId: "internet-store-d7a86",
+  storageBucket: "internet-store-d7a86.firebasestorage.app",
+  messagingSenderId: "74356785591",
+  appId: "1:74356785591:web:ce88ba2e7f795668496832"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
