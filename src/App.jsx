43170dc239677beaa6f76cfd94f5e800af32522e
m1rage <miragela.code@gmail.com>
2025-05-10 11:17:26 +0200

    Remove About page import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import Checkout from './components/Checkout';
